bfac52af846b1092251c09f8765e6fe6f6597e56
rzsamrio <samuelekop144@gmail.com>
2024-01-23 19:30:10 +0100

    Selection sort?

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,19 +8,29 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, tmp;
+	size_t i, j;
+	int pos, tmp, swap;
 
+	if (size < 2)
+		return;
 	for (i = 0; i < size; i++)
 	{
+		tmp = array[i];
+		swap = 0;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[i] > array[j])
+			if (tmp > array[j])
 			{
-				tmp = array[i];
-				array[i] = array[j];
-				array[j] = tmp;
+				tmp = array[j];
+				pos = j;
+				swap = 1;
 			}
 		}
-		print_array(array, size);
+		if (swap)
+		{
+			array[pos] = array[i];
+			array[i] = tmp;
+			print_array(array, size);
+		}
 	}
 }
